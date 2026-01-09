65e2c6ea7ca6fb18ae4749441bfd7f44f62f8928
Mark Pictor <mpictor@gmail.com>
2026-01-09 00:14:56 -0600

    update SPDX identifiers
    now matching the license change in b79a0d8da330
    
    rg -il spdx|xargs sed -i 's/SPDX-License-Identifier: BSD-3-Clause/SPDX-License-Identifier: MIT/'
    rg -l BSD-style | xargs sed -i 's|a BSD-style|an MIT-style|'

--- pkg/mfg/mfgflags/flags.go
+++ pkg/mfg/mfgflags/flags.go
@@ -1,8 +1,8 @@
 // Copyright (C) 2015-2020 the Gprovision Authors. All Rights Reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 //
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: MIT
 //
 
 // Package mfgflags handles flags used to alter mfg behavior for testing.
